3b29521add5ad59ded0500d8e67b83856a3ad20e
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-11-19 21:49:56 -0500

    first commit in node server

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import HelloController  from "./controllers/hello-controller.js";
+import UserController
+    from "./controllers/users/user-controller.js";
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+
+
+const app = express()
+app.use(express.json());
+app.use(cors());
+HelloController(app);
+UserController(app);
+TuitsController(app);
+app.listen(4000);
+
