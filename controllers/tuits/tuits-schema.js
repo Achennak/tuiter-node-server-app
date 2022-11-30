b35601cd8ad5a99d6d8ed7c297688b343d957f4e
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-11-30 16:22:38 -0500

    schema changes

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,18 +1,8 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
-                                   topic: String,
-                                   userName: String,
-                                    title: String,
-                                    time:String,
                                    tuit: String,
-                                   likes: String,
-                                   liked: Boolean,
-                                   image: String,
-                                   disliked: Boolean,
-                                   replies: String,
-                                   retuits: String,
-                                   dislikes: String,
-                                   handle: String
+                                   likes: Number,
+                                   liked: Boolean
                                }, {collection: 'tuits'});
 export default schema;
 
